f906a55328a73ace2f4492a5ef5d8a0faf7718e6
Daniel Burgess <dan-burgess@hotmail.com>
2023-07-13 12:23:50 +0930

    Dynamically render expense items

--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -1,4 +1,3 @@
-import {useState} from 'react'
 
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate'
@@ -6,19 +5,13 @@ import Card from '../UI/Card'
 
 const ExpenseItem = (props) => {
 
-  const [title, setTitle] = useState(props.title)
-
-  const handleClick = () => {
-    setTitle('Updated!')
-  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2> {title} </h2>
+        <h2> {props.title} </h2>
         <div className="expense-item__price"> {props.amount} </div>
       </div>
-      <button onClick={handleClick}>Change Title</button>
     </Card>
   )
 }
